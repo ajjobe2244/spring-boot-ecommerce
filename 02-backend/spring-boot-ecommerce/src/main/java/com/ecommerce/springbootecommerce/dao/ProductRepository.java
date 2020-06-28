bc5f5b398bba927cde91795e9fb494bc95a2758a
Ali Jobe <ali.jobe@virginpulse.com>
2020-06-27 23:40:48 -0400

    Added styling

--- 02-backend/spring-boot-ecommerce/src/main/java/com/ecommerce/springbootecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/ecommerce/springbootecommerce/dao/ProductRepository.java
@@ -2,7 +2,8 @@ package com.ecommerce.springbootecommerce.dao;
 
 import com.ecommerce.springbootecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
 }
